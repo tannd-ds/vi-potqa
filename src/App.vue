1ddd9d3136ef1ca1d69b292a7d50a821e10e212b
tannd-ds <donhattan1012dev@gmail.com>
2023-05-05 18:03:49 +0700

    simplify function, save data to LocalStorage

--- src/App.vue
+++ src/App.vue
@@ -4,23 +4,12 @@
 
   const ps = ref([
     {
-      id: 0,
       name: 'Hello World',
       content: [
-        {id: 0, content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'},
-        {id: 1, content: ' Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'},
-        {id: 2, content: ' Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'},
-        {id: 3, content: ' Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'},
-      ]
-    },
-    {
-      id: 1,
-      name: 'Hello World',
-      content: [
-        {id: 0, content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'},
-        {id: 1, content: ' Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'},
-        {id: 2, content: ' Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'},
-        {id: 3, content: ' Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'},
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        ' Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        ' Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+        ' Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
       ]
     },
   ])
@@ -29,23 +18,15 @@
   const question_content = ref("")
   const answer_content = ref("")
   const checked_ids = ref([])
+  const confirmed_data = Array(localStorage.getItem("data"))
 
   function para_to_sentences(para) {
-    let p_temp = []
-    let sentences = para.split(/(?<=[.!?])\s+/)
-    for (let i=0; i < sentences.length; i++) {
-      p_temp.push({
-        id: p_temp.length,
-        content: sentences[i],
-      })
-    }
-    return p_temp
+    return  para.split(/(?<=[.!?])\s+/)
   }
 
   function add() {
     if (hasValidInput()) {
         const p_temp = {
-          id: ps.value.length,
           name: p_name.value,
           content: para_to_sentences(p_content.value)
         }
@@ -66,14 +47,17 @@
       let p_id = ids[i].slice(0, ids[i].indexOf("-"))
       let s_id = ids[i].slice(ids[i].indexOf("-")+1)
       // Find the index of the Paragraph that has id = p_id
-      let p_index = ps.value.findIndex(p => p.id == p_id)
-      result.push([ps.value[p_index].name, Number(s_id)])
+      result.push([ps.value[p_id].name, Number(s_id)])
     }
     return result
   }
 
-  function remove_para(p_id) {
-    let index = ps.value.findIndex(p => p.id == p_id)
+  function find_index_a_in_b(a, b) {
+    return b.findIndex(b_element => b_element == a)
+  }
+
+  function remove_para(p) {
+    let index = find_index_a_in_b(p, ps.value)
     ps.value = ps.value.slice(0, index).concat(ps.value.slice(index+1))
   }
 
@@ -81,16 +65,20 @@
     let data = {}
     data['facts'] = ids_to_names(checked_ids)
     data['question'] = question_content.value
-    data['contexts'] = ps.value
+    let ps_simplified = []
+    for (let i = 0; i < ps.value.length; i++)
+      ps_simplified.push(ps.value[i].name, ps.value[i].content)
+    data['contexts'] = ps_simplified
     data['answer'] = answer_content.value
 
-    console.log(JSON.stringify(data))
+    confirmed_data.push(data)
+    localStorage.setItem("data", JSON.stringify(confirmed_data))
   }
+
 
 </script>
 
 <template>
-
   <div class="wrapper">
     <div class="left-panel">
       <div class="web-title-container">
@@ -127,28 +115,23 @@
             <h4 class="disable-select"> {{ p.name }} </h4>
             <div class="p-name-bar-btn">
               <button class="edit-btn" @click="" title="Edit Paragraph"><img src="./assets/pen-solid.svg"></button>
-              <!-- <button class="remove-btn" @click="remove_para(p.id)" title="Remove Paragraph"><img src="./assets/xmark-solid.svg"></button> -->
-              <button class="remove-btn" @click="remove_para(p.id)" title="Remove Paragraph"><img src="./assets/xmark-solid.svg"></button>
+              <button class="remove-btn" @click="remove_para(p)" title="Remove Paragraph"><img src="./assets/xmark-solid.svg"></button>
             </div>
           </div>
           <div class="s-confirmed" v-for="s in p.content">
             <input 
               type="checkbox" 
               name="optional" 
-              :id="String(p.id) + '-' + String(s.id)" 
-              :value="String(p.id) + '-' + String(s.id)" 
+              :id="String(find_index_a_in_b(p, ps)) + '-' + String(find_index_a_in_b(s, p.content))" 
+              :value="String(find_index_a_in_b(p, ps)) + '-' + String(find_index_a_in_b(s, p.content))" 
               v-model="checked_ids"
               required hidden
             >
-            <label :for="String(p.id) + '-' + String(s.id)" class="">{{ s.content }}</label>
+            <label :for="String(find_index_a_in_b(p, ps)) + '-' + String(find_index_a_in_b(s, p.content))" class="">{{ s }}</label>
           </div>
         </div>
       </div>
     </div>
 
   </div>
-  
 </template>
-
-<style scoped>
-</style>
