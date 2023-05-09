e7dd98702147e6e2c23829d95077d3a199e8563a
tannd-ds <donhattan1012dev@gmail.com>
2023-05-09 21:13:35 +0700

    change toast slide-transition

--- src/components/Toast.vue
+++ src/components/Toast.vue
@@ -54,20 +54,18 @@ const toast_icon_src = ref({
 <style scoped>
 
 .slide-fade-enter-active {
-  transition: all 0.3s ease-out;
   transform: translateX(0%);
+  transition: all 0.3s ease-out;
 }
 
 .slide-fade-leave-active {
-  transition: all 0.3s cubic-bezier(1, 0.5, 0.8, 1);
+  transition: all 0.2s cubic-bezier(1, 0.5, 0.8, 1);
 }
 
 .slide-fade-enter-from,
 .slide-fade-leave-to {
-  transform: translateX(0%);
+  transform: translateX(100%);
   opacity: 0;
-  -webkit-transform: scale(1.1);
-  transform: scale(1.1);
 }
 
 .toast-wrapper {
