bc444b5b68ac9e0e9c00ba20b931f8ac81236213
tannd-ds <donhattan1012dev@gmail.com>
2023-05-16 14:52:06 +0700

    re-program input fields into a component

---
+++ client/src/components/InputWithLabel.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="container">
+        <label 
+            class="disable-select" 
+            :for="id"
+        >
+            <slot name="label-name">{{label_name}}</slot>
+        </label>
+        <component
+            :is="type" 
+            :value="modelValue"
+            @input="$emit('update:modelValue', $event.target.value)"
+            :id="id" 
+            :placeholder="placeholder" 
+            :rows="type == `textarea` ? 10 : 1"
+            spellcheck="false" 
+            autocomplete="off" 
+            aria-autocomplete="none"
+        ></component>
+    </div>
+</template>
+
+<script setup>
+    defineProps({
+        'modelValue': String,
+        type: String,
+        id: String,
+        placeholder: String,
+        label_name: String,
+
+    })
+    defineEmits(['update:modelValue'])
+</script>
+
+<style scoped>
+.container {
+    display: flex;
+    flex-direction: column; 
+    gap: 0.5em;
+}
+
+.container input, textarea {
+    padding: 1.5em 1em;
+    background-color: rgba(255, 255, 255, 0.1);
+    border: transparent;
+    border-radius: var(--base-border-radius);
+    color: var(--primary-accent-color);
+    line-height: 1.5em;
+}
+
+.container input {
+    height: 2vh;
+}
+
+.container input:focus, textarea:focus {
+    outline: none;
+}
+
+
+</style>
