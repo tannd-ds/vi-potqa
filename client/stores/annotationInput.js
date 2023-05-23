3f0d91595f9072526d8035ce3daca0b384ac0c66
tannd-ds <donhattan1012dev@gmail.com>
2023-05-23 14:40:41 +0700

    fix updating checked_ids when edit contexts

--- client/stores/annotationInput.js
+++ client/stores/annotationInput.js
@@ -46,7 +46,7 @@ export const useAnnotationInputStore = defineStore('annotation_input', {
         },
         remove_context(context_id) {
             this.contexts = this.contexts.slice(0, context_id).concat(this.contexts.slice(context_id+1))
-            this.update_checked_id(context_id)
+            this.update_checked_id("remove", context_id)
         },
         show_edit_context(context_id) {
             const general_store = useGeneralStore()
@@ -54,32 +54,49 @@ export const useAnnotationInputStore = defineStore('annotation_input', {
             this.edit_context.name = this.contexts[context_id]['name']
             this.edit_context.content = this.contexts[context_id]['content'].join(" ")
             general_store.is_show_overlay = true
-            
         },
         save_edit_context() {
             const general_store = useGeneralStore()
-
             this.contexts[this.edit_context.original_index].name = this.edit_context.name
             this.contexts[this.edit_context.original_index].content = this.edit_context.content.split(/(?<=[.!?])\s+/)
             general_store.is_show_overlay = false
+            this.update_checked_id("edit", this.edit_context.original_index)
         },
-        update_checked_id(removed_p_id) {
-            let ids = this.checked_ids
+        update_checked_id(type="remove", context_id) {
             let result = []
-            for (let i = 0; i < ids.length; i++) {
-            // Split Paragraph id and Sentence id from id String
-            let p_id = ids[i].slice(0, ids[i].indexOf("-"))
-            let s_id = ids[i].slice(ids[i].indexOf("-") + 1 )
-            /*
-            - If p_id is removed_p_id: move on
-            - If p_id is less than remove_p_id: add it to new list, doesn't need extra update
-            - If p_id is greater than remove_p_id: update its index (by minus 1) then add it to new list
-            */
-            if (p_id == removed_p_id) continue
-            if (p_id < removed_p_id)
-                result.push(ids[i])
-            else 
-                result.push(`${p_id-1}-${s_id}`)
+            let ids = this.checked_ids
+            if (type == "remove") {
+                for (let i = 0; i < ids.length; i++) {
+                    // Split Paragraph id and Sentence id from id String
+                    let p_id = ids[i].slice(0, ids[i].indexOf("-"))
+                    let s_id = ids[i].slice(ids[i].indexOf("-") + 1 )
+                    /*
+                    - If p_id is context_id: move on
+                    - If p_id is less than context_id: add it to new list, doesn't need extra update
+                    - If p_id is greater than context_id: update its index (by minus 1) then add it to new list
+                    */
+                    if (p_id == context_id) continue
+                    if (p_id < context_id)
+                        result.push(ids[i])
+                    else 
+                        result.push(`${p_id-1}-${s_id}`)
+                }
+            } else if (type == "edit") {
+                for (let i = 0; i < ids.length; i++) {
+                    // Split Paragraph id and Sentence id from id String
+                    let p_id = ids[i].slice(0, ids[i].indexOf("-"))
+                    let s_id = ids[i].slice(ids[i].indexOf("-") + 1 )
+                    /*
+                    - If p_id is context_id: move on
+                    - If p_id is different than context_id: add it to new list
+                    */
+                    if (p_id == context_id) 
+                        continue
+                    else (p_id < context_id)
+                        result.push(ids[i])
+                }
+            } else {
+                prompt("update_checked_id has weird type")
             }
             this.checked_ids = result
         }
