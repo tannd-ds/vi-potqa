f7afcc65c3936acf9390c3dacfdb96f0f0616346
tannd-ds <donhattan1012dev@gmail.com>
2023-06-06 23:25:42 +0700

    Nuxt enable routing (pages)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    devtools: { enabled: true },
     modules: [
-        '@pinia/nuxt',
-        'nuxt-icon',
-        '@pinia-plugin-persistedstate/nuxt',
+      '@pinia/nuxt',
+      'nuxt-icon',
     ],
     css: [
        '@/assets/css/main.css'
     ],
 })
-
-import { createPinia } from 'pinia'
-import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
-
-export const pinia = createPinia()
-pinia.use(piniaPluginPersistedstate)
