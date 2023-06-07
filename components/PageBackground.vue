cedc32eea2e5a743ebff47726f2194aad028d5ca
tannd-ds <donhattan1012dev@gmail.com>
2023-06-07 08:11:33 +0700

    fix unbuildable by removing background light

--- components/PageBackground.vue
+++ components/PageBackground.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bg">
-    <img class="bg-gradient grad-1" src="../assets/images/Ellipse green-blue.png" />
-    <img class="bg-gradient grad-2" src="../assets/images/Ellipse pink.png" />
+    <!-- <img class="bg-gradient grad-1" src="../assets/images/Ellipse green-blue.png" /> -->
+    <!-- <img class="bg-gradient grad-2" src="../assets/images/Ellipse pink.png" /> -->
 
     <slot></slot>
   </div>
